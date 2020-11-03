9c22e5d3a064815d49b2f7476ce1e68c7fb5b16f
Yeening <yl7sr@virginia.edu>
2020-11-03 22:07:24 +0800

    Optimized functions

--- TimeCalculator.py
+++ TimeCalculator.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import openpyxl
 import sys
 
-RECORD_COLUMN = 14
+RECORD_COLUMN = 10
 
 def calculate(value):
     timeSum = 0
-    for time in value.split("\n"):
-        startT = time.split('-')[0]
-        endT = time.split('-')[1]
-        startM, startS = int(startT[:-2]), int(startT[-2:])
-        endM, endS = int(endT[:-2]), int(endT[-2:])
+    for time in value.strip().split("\n"):
+        time = time.strip()
+        startT = time.split('-')[0].strip()
+        endT = time.split('-')[1].strip()
+        startM, startS = int(startT[:-3]), int(startT[-2:])
+        endM, endS = int(endT[:-3]), int(endT[-2:])
+        # print(startM,endM)
         timeSum += (endM - startM) * 60 + (endS - startS)
     return timeSum
 
@@ -18,21 +22,21 @@ def isTime(s):
         return False
     if(s.split("\n")) is None:
         return False
-    return s.split("\n")[0].split('-')[0].isnumeric()
+    return s.strip().split("\n")[0].split('-')[0].isnumeric() or s.strip().split("\n")[0].split(':')[0].isnumeric()
 
 
 if __name__ == '__main__':
     if(len(sys.argv)>1):
         file_path = sys.argv[1]
     else:
-        file_path = 'EP4.xlsx'
+        file_path = 'EP9.xlsx'
     print("输入文件：",file_path)
     book = openpyxl.load_workbook(file_path)
     sheet = book.active
     # g6 = sheet['J15']
-    for i in range(3, 25):
+    for i in range(5, 17):
         curTime = 0
-        for j in range(1,14):
+        for j in range(1,10):
             cell = sheet.cell(row = i, column=j).value
             if(isTime(cell)):
                 curTime += calculate(cell)
